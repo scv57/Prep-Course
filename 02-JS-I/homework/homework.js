2c2255d01aa1e28236b8f636b147d23f007e947a
Santiago <santiagocristanchovallejo@gmail.com>
2022-02-16 19:06:31 -0500

    respuesta 0

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Esto es un String";
 const nuevoNum = 69;
 
 // Crea una variable booleana:
-const nuevoBool = 7.6;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4  === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
