369ccfb4b3b01dad74287233792afc40657f4565
Santiago <santiagocristanchovallejo@gmail.com>
2022-03-11 00:24:34 -0500

    Tareas principales completadas

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -25,12 +25,18 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = 0;
+  for(let i = 0; i < numeros.length; i++){
+      suma += numeros[i];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(element => cb(element))
 }
 
 function map(array, cb) {
@@ -38,12 +44,22 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+    const elArray = array.map(function(element){
+      return cb(element);
+    });
+    return elArray;
+
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const newArray = array.filter(Array => Array[0] === 'a');
+
+  return newArray;
+
+
 }
 
 // No modificar nada debajo de esta línea
